347cf238d147b720fca650e24f4ee3a6b10e86ae
kewl/fft <k@alex94.e4ward.com>
2017-12-02 14:27:49 +0000

    Update opennic.sh

--- opennic.sh
+++ opennic.sh
@@ -29,13 +29,18 @@ echo "Using $apihost as API host"
 
 # record my IP in whitelist if my account login parameters have been passed: userid and keyid
 if [ $# -eq 2 ]; then
-    curl --resolve "api.opennicproject.org:443:$apihost" "https://api.opennicproject.org/ip/update/?user=$1&auth=$2"
+    curl --connect-timeout 60 --resolve "api.opennicproject.org:443:$apihost" "https://api.opennicproject.org/ip/update/?user=$1&auth=$2"
 fi
 
 # query the API for 200 sites
 apiurl="https://api.opennicproject.org/geoip/?list&ipv=4&res=200&adm=0&bl&wl"
 echo $apiurl
-hosts=$(curl --silent --resolve "api.opennicproject.org:443:$apihost" $apiurl)
+hosts=$(curl --silent --connect-timeout 60 --resolve "api.opennicproject.org:443:$apihost" $apiurl)
+
+if [ -z "$hosts" ]; then
+  echo "API not available" 1>&2
+  exit 1
+fi
 
 # filter hosts with more than 90% reliability
 allhosts=$(echo "$hosts" | awk -F# '$3 + 0.0 > 90' | awk -F# '{print $1}')
@@ -46,29 +51,21 @@ if [ "$allhostscount" -ge 2 ]; then
   echo "Pinging $allhostscount hosts to determine the top ones..."
   pingresults=$(multiping $allhosts)
 
-  # We throw away servers that fall below the average packet loss of all servers
-  # Illustration of packet loss filter:
-  # 5 servers: #1 received 1 response, #2 received 2 responses, #3 received 3 responses, #4 = 4, #5 = 5
-  # (1+2+3+4+5)/5 = 3, the average amount of responses per server is 3
-  # we filter out all servers that have a response packet count of below 3 (in this case #1 and #2 fall out of our list; #3, #4 and #5 are the servers we're going to test)
-  avgrcvd=$(echo "$pingresults" | awk -F/ 'BEGIN{sum=0;count=0;}{count+=1;sum+=$4}END{print sum/count;}')
-
-  # Here we will finally apply the packet loss filter and also sort the servers by their average response time
-  hosts=$(echo "$pingresults" | awk -F/ '$4 >= '$avgrcvd'' | sort -t/ -nk8)
+  # we apply the packet loss filter and also sort the servers by their average response time and keep only the IP column
+  hosts=$(echo "$pingresults" | awk -F/ '$5 + 0.0 < 10' | sort -t/ -nk8 | awk '{print $1}')
   hostscount=$(echo "$hosts" | wc -l)
   echo "Resulting in $hostscount responsive hosts"
 
-  # we retain the top 3 servers for our DNS and keep only the IP column
-  myhosts=$(echo "$hosts" | head -n 3 | awk '{print $1}')
-  echo $myhosts
-
   # replace Network Manager DNS with the new ones for all active connections
   if [ "$hostscount" -ge 2 ]; then
+    # we retain the top 3 servers for our DNS
+    myhosts=$(echo "$hosts" | head -n 3)
+    echo $myhosts
+
     for id in $(nmcli -terse -fields UUID connection show --active)
     do
       currentdnss=$(nmcli -terse -fields ipv4.dns connection show $id | cut -d: -f2- | tr "," "\n")
-      if [ "$(echo "$currentdnss" | sort)" == "$(echo "$myhosts" | sort)" ]
-      then
+      if [ "$(echo "$currentdnss" | sort)" == "$(echo "$myhosts" | sort)" ]; then
           echo "No dns change"
       else
           #statements
