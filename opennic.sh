6c0cbea50efcbe5da96d15f773b67a93d501ceba
kewl/fft <k@alex94.e4ward.com>
2017-12-02 11:50:00 +0000

    Update opennic.sh

--- opennic.sh
+++ opennic.sh
@@ -39,48 +39,57 @@ hosts=$(curl --silent --resolve "api.opennicproject.org:443:$apihost" $apiurl)
 
 # filter hosts with more than 90% reliability
 myhosts=$(echo "$hosts" | awk -F# '$3 + 0.0 > 90' | awk -F# '{print $1}')
+myhostscount=$(echo "$myhosts" | wc -l)
 
-#pinging the hosts
-echo "Pinging $(echo "$myhosts" | wc -l) hosts to determine the top ones..." 1>&2
-pingresults=$(multiping $myhosts)
+if [ "$myhostscount" -ge 2 ]; then
 
-# We throw away servers that fall below the average packet loss of all servers
-# Illustration of packet loss filter:
-# 5 servers: #1 received 1 response, #2 received 2 responses, #3 received 3 responses, #4 = 4, #5 = 5
-# (1+2+3+4+5)/5 = 3, the average amount of responses per server is 3
-# we filter out all servers that have a response packet count of below 3 (in this case #1 and #2 fall out of our list; #3, #4 and #5 are the servers we're going to test)
-avgrcvd=$(echo "$pingresults" | awk -F/ 'BEGIN{sum=0;count=0;}{count+=1;sum+=$4}END{print sum/count;}')
+  #pinging the hosts
+  echo "Pinging $myhostscount hosts to determine the top ones..." 1>&2
+  pingresults=$(multiping $myhosts)
 
-# Here we will finally apply the packet loss filter and also sort the servers by their average response time
-hosts=$(echo "$pingresults" | awk -F/ '$4 >= '$avgrcvd'' | sort -t/ -nk8)
-hostscount=$(echo "$hosts" | wc -l)
-echo "Resulting in $hostscount responsive hosts"
+  # We throw away servers that fall below the average packet loss of all servers
+  # Illustration of packet loss filter:
+  # 5 servers: #1 received 1 response, #2 received 2 responses, #3 received 3 responses, #4 = 4, #5 = 5
+  # (1+2+3+4+5)/5 = 3, the average amount of responses per server is 3
+  # we filter out all servers that have a response packet count of below 3 (in this case #1 and #2 fall out of our list; #3, #4 and #5 are the servers we're going to test)
+  avgrcvd=$(echo "$pingresults" | awk -F/ 'BEGIN{sum=0;count=0;}{count+=1;sum+=$4}END{print sum/count;}')
 
-# we retain the top 3 servers for our DNS and keep only the IP column
-hostsshortlist=$(echo "$hosts" | head -n 3 | awk '{print $1}')
-echo "$hostsshortlist"
+  # Here we will finally apply the packet loss filter and also sort the servers by their average response time
+  hosts=$(echo "$pingresults" | awk -F/ '$4 >= '$avgrcvd'' | sort -t/ -nk8)
+  hostscount=$(echo "$hosts" | wc -l)
+  echo "Resulting in $hostscount responsive hosts"
 
-# replace Network Manager DNS with the new ones for all active connections
-if [ "$hostscount" -ge 2 ]; then
-  for id in $(nmcli -terse -fields UUID connection show --active)
-  do
-    currentdnss=$(nmcli -terse -fields ipv4.dns connection show $id | cut -d: -f2- | tr "," "\n")
-    if [ "$(echo "$currentdnss" | sort)" == "$(echo "$hostsshortlist" | sort)" ]
-    then
-        echo "No dns change"
-    else
-        #statements
-        for dns in $currentdnss
-        do
-          nmcli connection modify $id -ipv4.dns $dns
-        done
+  # we retain the top 3 servers for our DNS and keep only the IP column
+  hostsshortlist=$(echo "$hosts" | head -n 3 | awk '{print $1}')
+  echo "$hostsshortlist"
 
-        for dns in $hostsshortlist
-        do
-          nmcli connection modify $id +ipv4.dns $dns
-        done
-        echo "Updating $id"
-        nmcli connection up $id
-    fi
-  done
+  # replace Network Manager DNS with the new ones for all active connections
+  if [ "$hostscount" -ge 2 ]; then
+    for id in $(nmcli -terse -fields UUID connection show --active)
+    do
+      currentdnss=$(nmcli -terse -fields ipv4.dns connection show $id | cut -d: -f2- | tr "," "\n")
+      if [ "$(echo "$currentdnss" | sort)" == "$(echo "$hostsshortlist" | sort)" ]
+      then
+          echo "No dns change"
+      else
+          #statements
+          for dns in $currentdnss
+          do
+            nmcli connection modify $id -ipv4.dns $dns
+          done
+
+          for dns in $hostsshortlist
+          do
+            nmcli connection modify $id +ipv4.dns $dns
+          done
+          echo "Updating $id"
+          nmcli connection up $id
+      fi
+    done
+  else
+    echo "Not enough responsive OpenNIC servers available"
+  fi
+
+else
+  echo "Not enough OpenNIC servers available"
 fi
